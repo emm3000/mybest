a9b36554750f724b1de6a3846340212e0f69f721
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-01-22 10:58:05 -0500

    Add Firebase configuration and dependencies
    - Add `google-services.json` configuration file.
    - Add Firebase BOM dependency to `libs.versions.toml` and `app/build.gradle.kts`.
    - Register and apply `com.google.gms.google-services` plugin in project and app build files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -73,4 +74,6 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+
+    implementation(platform(libs.firebase.bom))
 }
